d1c4fc58935e29d154fa1b3573456fd38e1629e0
maaraa <maaraa@dotworld.in>
2017-09-09 14:41:21 +1000

    Added Check Group & Check CPU

--- src/chkDateTime.py
+++ src/chkDateTime.py
@@ -1,10 +1,56 @@
 from datetime import datetime, date
+import paramiko
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+from pysnmp.proto.rfc1902 import Integer, IpAddress, OctetString
 
-def chkDateTime(devIP, uName, passWord):
-    print("checkin the status...")
+
+def chkmarapara(devIP, uName, passWord, community, threshold):
+    usergroupstatus = chkUserGroup(uName)
+    cpustatus = chkDeviceCPU(devIP, community, threshold)
+
+    if usergroupstatus == "OK" and cpustatus == "OK":
+        return "OK:Arista-AVPair = 'shell:roles=network-admin', Service-Type = NAS-Prompt-User"
+    else:
+        return "ERR"
+
+
+def chkUserGroup(uName):
+    whatsUpGroup = "nomatch"
+    f = open ("../dbs/ugroup", "r")
+    for i in f:
+        sp = i.split(":")
+        if uName == sp[0]:
+            whatsUpGroup = sp[1].rstrip('\r\n')
+            break
+    if whatsUpGroup == 'admin':
+        status = 'OK'
+    elif whatsUpGroup == 'ronly':
+        status = 'ERR'
+    elif whatsUpGroup == 'rwonly':
+        status = 'ERR'
+    else :
+        status = 'INVALID QUERY'
     return status
 
-def fetchUserGroup(uName):
-    f = open ("ugroup", "r")
-    for i in f:
-        print(i)
+def chkDeviceCPU(devIP, community, threShold):
+    cpuUsage = 100
+    oid = "1.3.6.1.2.1.25.3.3.1.2"
+    community = community
+    version = 1
+    generator = cmdgen.CommandGenerator()
+    comm_data = cmdgen.CommunityData('server', community, version)
+    transport = cmdgen.UdpTransportTarget((devIP, 161))
+    rf = getattr(generator, 'nextCmd')
+    res = (errorIndication, errorStatus, errorIndex, varBinds) = rf(comm_data, transport, oid)
+    if not errorIndication is None  or errorStatus is True:
+       print("Error: %s %s %s %s" % res)
+    else:
+        # print("%s" % type(varBinds))
+        for i in varBinds:
+            for j in i:
+                cpuUsage = str(j).split(" ")[2]
+                break
+    if int(cpuUsage) <= threShold:
+        return "OK"
+    else:
+        return "ERR "
---
+++ src/maaraaTest.py
@@ -0,0 +1,7 @@
+import chkDateTime as cdt
+
+def testDateTime():
+    result = cdt.chkmarapara("13.210.82.184", "ntc", "ntc123", "networktocode", 20)
+    # cpuresult = cdt.chkDeviceCPU("13.210.82.184", "ntc", "ntc123", 15)
+    print(result)
+testDateTime()
