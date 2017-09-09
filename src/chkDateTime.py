5b32f5597e744b65589dad0451ec024c70967fe5
maaraa <maaraa@dotworld.in>
2017-09-09 11:47:53 +1000

    Datetime commit

---
+++ src/chkDateTime.py
@@ -0,0 +1,10 @@
+from datetime import datetime, date
+
+def chkDateTime(devIP, uName, passWord):
+    print("checkin the status...")
+    return status
+
+def fetchUserGroup(uName):
+    f = open ("ugroup", "r")
+    for i in f:
+        print(i)
